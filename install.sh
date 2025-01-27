b88173b48bba1ede1c825a3e19c1b622ac11718e
Shivam583-hue <shivamshivamshivam456@gmail.com>
2025-01-27 17:39:20 +0600

    install script fixed

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 TARGET_DIR="/usr/local/bin"
-BIN_NAME="kanban-board-cli"  # Match your actual binary name
+BIN_NAME="my-todo-cli"  # Name of the binary inside the .tar.gz
+VERSION="1.0.0"
 
 # Detect OS and architecture
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
@@ -10,11 +11,12 @@ ARCH=$(uname -m)
 # Map architectures
 case "$ARCH" in
   "x86_64") ARCH="amd64" ;;
-  "arm64") ARCH="arm64" ;;  # For Apple Silicon
+  "arm64") ARCH="arm64" ;;
+  *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
 esac
 
-# Updated URL
-DOWNLOAD_URL="https://github.com/Shivam583-hue/kanban-board-cli-/releases/download/v1.0.0/kanban-board-cli-$OS-$ARCH.tar.gz"
+# Download URL
+DOWNLOAD_URL="https://github.com/Shivam583-hue/kanban-board-cli-/releases/download/v1.0.0/my-todo-cli-$VERSION-$OS-$ARCH.tar.gz"
 
 echo "Installing $BIN_NAME..."
 curl -L $DOWNLOAD_URL | tar -xz -C $TARGET_DIR $BIN_NAME
